2b3d53cbbd916662712410c611c5d7fb97c6d231
Akhil G <akhileshg56972@stu.powayusd.com>
2020-10-29 20:27:54 +0000

    quizzes page

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 import os
 import jinja2
 
 #create a Flask instance
 app = Flask(__name__)
+
 
 #connects default URL to a function
 @app.route('/')
@@ -11,20 +12,27 @@ def home():
     #Flask import uses Jinga to render HTML
     return render_template("home.html")
 
+
 #connect to videos page
 @app.route('/videos')
 def video():
-  return render_template("videos.html")
+    return render_template("videos.html")
+
 
 #connects to calculator page
 @app.route('/calculator')
 def calulator():
-  return render_template("calculator.html")
+    return render_template("calculator.html")
+
 
 #connects to journals page
 @app.route('/journals')
 def journals():
-  return render_template("journals.html")
+    return render_template("journals.html")
+
+@app.route('/quizzes')
+def quizzes():
+   return render_template("quizzes.html")
 
 if __name__ == "__main__":
     #runs the application on the repl development server
