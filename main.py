c7e6f22b51eff213039514d9aa4ab9ba10335452
Akhil G <55467771+AkhileshLG@users.noreply.github.com>
2020-11-17 22:54:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def quizzes():
 
 if __name__ == "__main__":
     #runs the application on the repl development server
-    app.run(debug=True, port='5000', host='127.0.0.1')
+    app.run(port='3000', host='0.0.0.0')
     #KK
     #Karam
